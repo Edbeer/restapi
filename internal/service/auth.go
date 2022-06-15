db1c47cdc47651fec2c93f35c8d44d66fb42a9fd
Edbeer <edbeermtn@gmail.com>
2022-06-16 07:27:38 +0800

    refacroting service

--- internal/service/auth.go
+++ internal/service/auth.go
@@ -2,20 +2,18 @@ package service
 
 import (
 	"github.com/Edbeer/restapi/config"
-	"github.com/Edbeer/restapi/pkg/logger"
 	"github.com/Edbeer/restapi/internal/storage"
 )
 
 // Auth service
 type AuthService struct {
-	logger *logger.Logger
-	config *config.Config
-	storage storage.AuthStorage
+	config  *config.Config
+	storage storage.Auth
 }
 
 // Auth service constructor
-func NewAuthService(logger *logger.Logger, config *config.Config, storage storage.AuthStorage) *AuthService {
-	return &AuthService{logger: logger, config: config, storage: storage}
+func NewAuthService(config *config.Config, storage storage.Auth) *AuthService {
+	return &AuthService{config: config, storage: storage}
 }
 
 // Create new user
--- internal/service/service.go
+++ internal/service/service.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"github.com/Edbeer/restapi/config"
 	"github.com/Edbeer/restapi/internal/storage"
 )
 
@@ -14,12 +15,13 @@ type Services struct {
 }
 
 type Deps struct {
-
+	Storage *storage.Storage
+	Config  *config.Config
 }
 
 func NewService(deps Deps) *Services {
-	authStorage := storage.NewAuthStorage()
+	authService := NewAuthService(deps.Config, deps.Storage.Auth)
 	return &Services{
-		Auth: authStorage,
+		Auth: authService,
 	}
 }
